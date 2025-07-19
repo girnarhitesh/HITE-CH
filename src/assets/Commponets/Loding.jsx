e2eb663047c92672241770d194c69bb040beacfe
girnarhitesh <hiteshgirnar99@gmail.com>
2025-07-19 14:08:45 +0530

    loding  code change

--- src/assets/Commponets/Loding.jsx
+++ src/assets/Commponets/Loding.jsx
@@ -4,8 +4,10 @@ import gsap from 'gsap';
 
 function Loding() {
     useEffect(() => {
-        const tl = gsap.timeline();
+    const tl = gsap.timeline();
 
+    // Ensure elements are mounted before animating
+    requestAnimationFrame(() => {
         tl.fromTo(
             '.loader-logo',
             { y: 50, opacity: 0 },
@@ -16,8 +18,8 @@ function Loding() {
             delay: 0.5,
             ease: 'power2.inOut'
         });
-    }, []);
-
+    });
+}, []);
     return (
         <div className="loader-container">
             <div className="loader-logo">HITE-CH</div>
