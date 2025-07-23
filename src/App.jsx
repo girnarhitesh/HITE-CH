c3f16689c058a92880abb5db3ba1b0e0d75e90a9
girnarhitesh <hiteshgirnar99@gmail.com>
2025-07-23 11:46:31 +0530

    app code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdOutlineVerticalAlignTop } from 'react-icons/md';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './assets/Commponets/AllHomepages/Navigation/Navigation';
@@ -21,7 +22,27 @@ import Services from './assets/Commponets/Services/Services';
 import Footer from './assets/Commponets/Footer/Footer';
 import Contact from './assets/Commponets/Contact/Contact';
 
+
 function App() {
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowButton(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <BrowserRouter>
       <Navigation />
@@ -35,16 +56,22 @@ function App() {
         <Route path='/whoweare' element={<Whoweare />} />
         <Route path='/standards' element={<Standards />} />
         <Route path='/machinery' element={<Machinery />} />
-        <Route path='/formwork' element={<Formwork/>}/>
-        <Route path='/technology' element={<Tecnology/>}/>
-        <Route path='/residential' element={<Residential/>}/>
-        <Route path='/commercial' element={<Commercial/>}/>
-        <Route path='industrial' element={<Industrial/>}/>
-        <Route path='/institutional' element={<Institutional/>}/>
-        <Route path='/services' element={<Services/>}/>
-        <Route path='contact' element={<Contact/>}/>
+        <Route path='/formwork' element={<Formwork />} />
+        <Route path='/technology' element={<Tecnology />} />
+        <Route path='/residential' element={<Residential />} />
+        <Route path='/commercial' element={<Commercial />} />
+        <Route path='industrial' element={<Industrial />} />
+        <Route path='/institutional' element={<Institutional />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='contact' element={<Contact />} />
       </Routes>
-      <Footer/>
+      <Footer />
+
+      showButton && (
+      <div className="back-to-top" onClick={scrollToTop}>
+        <MdOutlineVerticalAlignTop size={30} />
+      </div>
+      )
     </BrowserRouter>
   );
 }
